9460f3f9c83d58bd70e35eef3f45410d23e504d2
wenx29 <wenx29@mcmaster.ca>
2021-10-11 15:05:10 -0400

    add Press to Exit

--- 02_getting_an_image_on_the_screen/02_getting_an_image_on_the_screen.cpp
+++ 02_getting_an_image_on_the_screen/02_getting_an_image_on_the_screen.cpp
@@ -25,7 +25,7 @@ SDL_Window* gWindow = NULL;
 SDL_Surface* gScreenSurface = NULL;
 
 //The image we will load and show on the screen
-SDL_Surface* gHelloWorld = NULL;
+SDL_Surface* gXOut = NULL;
 
 bool init()
 {
@@ -63,8 +63,8 @@ bool loadMedia()
 	bool success = true;
 
 	//Load splash image
-	gHelloWorld = SDL_LoadBMP( "02_getting_an_image_on_the_screen/hello_world.bmp" );
-	if( gHelloWorld == NULL )
+	gXOut = SDL_LoadBMP( "02_getting_an_image_on_the_screen/hello_world.bmp" );
+	if( gXOut == NULL )
 	{
 		printf( "Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError() );
 		success = false;
@@ -76,8 +76,8 @@ bool loadMedia()
 void close()
 {
 	//Deallocate surface
-	SDL_FreeSurface( gHelloWorld );
-	gHelloWorld = NULL;
+	SDL_FreeSurface( gXOut );
+	gXOut = NULL;
 
 	//Destroy window
 	SDL_DestroyWindow( gWindow );
@@ -103,19 +103,26 @@ int main( int argc, char* args[] )
 		}
 		else
 		{
-			//Apply the image
-			SDL_BlitSurface( gHelloWorld, NULL, gScreenSurface, NULL );
-			
-			//Update the surface
-			SDL_UpdateWindowSurface( gWindow );
+			bool quit = false;
 
-			//Wait two seconds
-			SDL_Delay( 2000 );
+			SDL_Event e;
+
+			while (!quit)
+			{
+				while (SDL_PollEvent(&e) != 0)
+				{
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
+				}
+				//Apply the image
+				SDL_BlitSurface(gXOut, NULL, gScreenSurface, NULL);
+
+				//Update the surface
+				SDL_UpdateWindowSurface(gWindow);
+			}
 		}
 	}
-
-	//Free resources and close SDL
-	close();
-
 	return 0;
 }
